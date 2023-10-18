314d8e27ec067c0e951106523506fff43ba468cf
Zeniittt <tinnqd1702@gmail.com>
2023-10-18 14:48:20 +0700

    add littleClass (commit vội, do quên chia branch, thiếu chuyên nghiệp :(()

---
+++ DemoACadSharp/AcadEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoACadSharp
+{
+    public class AcadEntity
+    {
+        private int? id;
+        private string layerName;
+        private string objectType;
+        private List<string> coordinates = null;
+
+        public int? Id { get => id; set => id = value; }
+        public string LayerName { get => layerName; set => layerName = value; }
+        public string ObjectType { get => objectType; set => objectType = value; }
+        public List<string> Coordinates { get => coordinates; set => coordinates = value; }
+    }
+}
---
+++ DemoACadSharp/Architecture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoACadSharp
+{
+    public class Architecture
+    {
+        string nameArchitecture;
+        int numberOfFloor;
+        string typeOfRoof;
+        List<Floor> document;
+
+        public string NameArchitecture { get => nameArchitecture; set => nameArchitecture = value; }
+        public int NumberOfFloor { get => numberOfFloor; set => numberOfFloor = value; }
+        public string TypeOfRoof { get => typeOfRoof; set => typeOfRoof = value; }
+        public List<Floor> Document { get => document; set => document = value; }
+    }
+}
